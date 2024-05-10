22a9a9180e2aa2289cef233d2543c6a359c68545
ntphuong120 <nguyenphuong11209@gmail.com>
2024-05-10 15:46:33 +0700

    Them Phep Cong

---
+++ code.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void print(const string &msg, const float &res) {
+    cout << msg << ": " << res;
+}
+
+int phepCong(const int &a, const int &b) {
+    return a + b;
+}
+
+int main() {
+    int a = 2, b = 5;
+    print("Phep Cong", phepCong(a, b));
+}
