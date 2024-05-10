e0b231740a0622931f282410d865177bfc077469
ntphuong120 <123523923+ntphuong120@users.noreply.github.com>
2024-05-10 15:59:39 +0700

    new feature

--- code.cpp
+++ code.cpp
@@ -11,7 +11,14 @@ int phepCong(const int &a, const int &b) {
     return a + b;
 }
 
+int phepNhan(const int &a, const int &b) {
+    return a * b;
+}
+
 int main() {
     int a = 2, b = 5;
     print("Phep Cong", phepCong(a, b));
+
+    // Phep Nhan
+    print("Phep Nhan", phepNhan(a, b));
 }
