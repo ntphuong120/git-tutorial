386a6614cef62c2f59795003cf128386fadfd7ba
ntphuong120 <nguyenphuong11209@gmail.com>
2024-05-10 15:52:39 +0700

    Them Phep Chia

---
+++ phepchia/phepchia.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+void print(const string &msg, const float &res) {
+    cout << msg << ": " << res << '\n';
+}
+
+float phepchia(const float &a, const float &b) {
+    return a/b;
+}
+
+int main() {
+    float a = 10, b = 4;
+
+    print("Phep Chia", phepchia(a, b));
+}
